3f3bc4eaa432b151308c44a1e90673e4c7104536
BerlinProsper <berlinprosper@gmail.com>
2025-07-22 23:40:06 +0530

    Initial commit

---
+++ src/data/constants.js
@@ -0,0 +1,246 @@
+export const Bio = {
+  name: "Berlin Prosper",
+  roles: [
+    "Full Stack Developer",
+    "Programmer",
+    "ReactJS Developer"
+  ],
+  description:
+  "BTech Computer Science graduate with hands-on experience in full-stack development using React.js, Node.js, and Firebase. Skilled in building responsive apps, integrating APIs, and deploying on AWS and Firebase. Passionate about solving real-world problems with clean, scalable code.", 
+  github: "https://github.com/BerlinProsper",
+  resume:
+  "https://drive.google.com/file/d/16h2vMkS5QXAYkNM06DYML0xH_lm-iIwX/view?usp=drivesdk",  
+  linkedin: "https://www.linkedin.com/in/berlin-p/",
+  insta: "https://www.instagram.com/storiesofberry/",
+  facebook: "https://www.facebook.com/Berlinprosper/",
+};
+
+export const skills = [
+  {
+    title: "Frontend",
+    skills: [
+      {
+        name: "React Js",
+        image:
+          "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+      },
+
+      {
+        name: "HTML",
+        image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+      },
+      {
+        name: "CSS",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+      },
+      {
+        name: "JavaScript",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+      },
+ 
+      {
+        name: "Material UI",
+        image:
+          "https://raw.githubusercontent.com/BerlinProsper/myPhoto/main/ui.png"      },
+
+    ],
+  },
+  {
+    title: "Backend",
+    skills: [
+      {
+        name: "Node Js",
+        image: "https://nodejs.org/static/images/logo.svg",
+      },
+      {
+        name: "Express Js",
+        image:
+         "https://raw.githubusercontent.com/BerlinProsper/myPhoto/main/express.PNG",      },
+     
+      {
+        name: "MySQL",
+        image:
+         "https://raw.githubusercontent.com/BerlinProsper/myPhoto/main/mysql.PNG"      },
+      {
+        name: "Firebase",
+        image:"https://raw.githubusercontent.com/BerlinProsper/myPhoto/main/firebase.PNG",
+      },
+        
+        {
+        name: "AWS S3",
+        image:
+          "https://raw.githubusercontent.com/BerlinProsper/myPhoto/main/aws%20s3.PNG"      }
+      
+ 
+    ],
+  },
+  {
+    title: "Android",
+    skills: [
+      {
+        name: "Java",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/java/java-original.svg",
+      },
+      
+    ],
+  },
+  {
+    title: "Others",
+    skills: [
+      {
+        name: "Git",
+        image:
+          "https://raw.githubusercontent.com/BerlinProsper/myPhoto/main/git.PNG",
+      },
+      {
+        name: "GitHub",
+        image:
+          "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+      },
+     
+      {
+        name: "VS Code",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+      },
+      {
+        name: "Postman",
+        image:
+         
+      "https://raw.githubusercontent.com/BerlinProsper/myPhoto/main/postman.WEBP",},
+     
+    ],
+  },
+];
+
+export const experiences = [
+  {
+    id: 0,
+    img: "https://raw.githubusercontent.com/BerlinProsper/myPhoto/main/IMG_2519.PNG",
+    role: "Fullstack Engineering Intern",
+    company: "BillTrim",
+    date: "Sept 2022 - Dec 2022",
+   skills: [
+      "JavaScript",
+      "Node Js",
+    ],
+    doc: "https://media.licdn.com/dms/image/D4D2DAQFlp60ZqHuaFQ/profile-treasury-image-shrink_1280_1280/0/1691180828512?e=1692381600&v=beta&t=mM5Y_NE5EPlQhez5FAN6NLVSKcO_Ojt_9Gq3mnFGkAQ",
+  },
+ 
+];
+
+export const education = [
+  {
+    id: 0,
+    img: "https://raw.githubusercontent.com/BerlinProsper/myPhoto/main/IMG_2529.JPG",
+    school: "University college of engineering, Kerala",
+    date: "Oct 2018 - Sep 2022",
+    grade: "6.5 CGPA",
+    desc: "I pursued my Bachelor’s degree in Computer Science and Engineering at the University College of Engineering, Kariavattom. Over 8 semesters, I gained a solid foundation in core subjects such as Data Structures, Algorithms, Object-Oriented Programming, Database Management Systems, Operating Systems, and Computer Networks. My time at the university helped me strengthen my technical skills and deepen my understanding of computer science fundamentals.",
+        degree: "Bachelor of Technology - BTech, Computer Science and Engineering",
+  },
+  {
+    id: 1,
+    img: "https://raw.githubusercontent.com/BerlinProsper/myPhoto/main/IMG_2530.JPG",
+    school: "St Francis sales school",
+    date: "Apr 2016 - Apr 2018",
+    grade: "72.5%",
+    desc: "I completed my class 12 high chool education at ST francis sales school",
+    degree: "CBSE(XII), Science",
+  },
+  {
+    id: 2,
+    img: "https://raw.githubusercontent.com/BerlinProsper/myPhoto/main/IMG_2530.JPG",
+    school: "St Francis sales school",
+    date: "Apr 2015 - Apr 2016",
+    grade: "90%",
+    desc: "I completed my class 10 education at St Francis sales school where I studied Science with Computer Application.",
+    degree: "CBSE(X), Science with Computer",
+  },
+];
+
+export const projects = [
+  
+  {
+    id: 0,
+    title: "Student Form",
+    date: "Feb 2023",
+    description:
+    "Made Fullstack StudentForm using ReactJS, Java and MongoDB",
+      image:
+    "https://raw.githubusercontent.com/BerlinProsper/myPhoto/main/screenshot3.webp",
+      tags: [
+      "Java",
+      "React Js",
+      "MongoDb",
+    
+    ],
+    category: "app",
+    github: "https://github.com/BerlinProsper/studentportal-ui",
+    webapp:""
+
+  },
+   {
+    id: 1,
+    title: "BookMyShow Clone",
+    date: "Dec 2024",
+    description:
+   "This version of the Book My Show app uses React.js for the frontend and Express.js with MySQL for the backend. It features user/admin portals.",
+    image:
+    "https://raw.githubusercontent.com/BerlinProsper/myPhoto/main/IMG_2519.PNG", 
+
+    tags: [
+      "React Js",
+      "Express Js",
+      "MySQL"
+    
+    ],
+    category: "app",
+    github: "https://github.com/BerlinProsper/BookMyShowBackend.git",
+    webapp:""
+
+  }, {
+    id: 2,
+    title: "Expense Tracker",
+    date: "Feb 2024",
+    description:
+    "A personal finance management tool developed using React.js and Firebase. Users can log expenses, categorize transactions, and track their spending patterns.",
+      image:
+    "https://raw.githubusercontent.com/BerlinProsper/myPhoto/main/IMG_2520.PNG",
+      tags: [
+      "React Js",
+      "Firebase Auth",
+      "Firebase Realtime Database"
+    
+    ],
+    category: "web app",
+    github: "https://github.com/BerlinProsper/ExpenseTrackers.git",
+    webapp:"https://expensetracker-6126e.web.app/signin"
+
+  }, {
+    id: 3,
+    title: "BookMyShow with Firebase",
+    date: "May 2024",
+    description:
+   "A full-stack clone of the BookMyShow ticket booking platform built using React.js and Firebase.",
+  image:
+    "https://raw.githubusercontent.com/BerlinProsper/myPhoto/main/IMG_2519.PNG", 
+     tags: [
+      "React Js",
+      "Firebase Auth",
+      "Firebase Realtime Database"
+    
+    ],
+    category: "web app",
+    github: "https://github.com/BerlinProsper/BookMyShow.git",
+    webapp: "https://bookmyshow-a3ca3.web.app/",
+
+  },];
+export const TimeLineData = [
+  { year: 2018, text: "Started my journey" },
+  { year: 2022, text: "Worked as Intern" },
+  { year: 2023-2025, text: "Did some projects as Freelancer"}
+];
---
+++ src/utils/Themes.js
@@ -0,0 +1,22 @@
+export const darkTheme = {
+    bg:"#1c1c27",
+    bgLight: "#845ce6",
+    primary:"#f2f3f4",
+    text_primary:"#f2f3f4",
+    text_secondary:"#b1b2b3",
+    card:"#171721",
+    card_light: '#1919294',
+    button:"#845ce6",
+    white:"#FFFFFF",
+    black:"#000000",
+}
+
+export const lightTheme = {
+    bg:"#FFFFFF",
+    bgLight: "#f0f0f0",
+    primary:"#be1adb",
+    text_primary:"#111111",
+    text_secondary:"#48494a",
+    card:"#FFFFFF",
+    button:"#5c5b5b",
+}
